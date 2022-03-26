f07db8f74e2b71b042fd31314b6ce34e06be307e
Martin Infante <maleinf09@gmail.com>
2022-03-26 18:56:44 -0300

    Add basic Owner model

--- web/models.py
+++ web/models.py
@@ -6,15 +6,31 @@ class Client(models.Model):
     This model defines the Client table.
 
     fields:
-        - email(str): This field must be unique
+        - email(str):
         - first_name(str):
         - last_name(str):
-        - identity_number(int): This field must be unique
-        - approved(bool): Profile approved by owner or not.
+        - identity_number(int):
 
     """
     email = models.CharField(unique=True, max_length=50)
+    password = models.CharField(max_length=20, null=False)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     identity_number = models.IntegerField(unique=True)
-    approved = models.BooleanField()
+
+
+class Owner(models.Model):
+    """
+    This model defines the Owner table.
+
+    fields:
+        - owner_id(int): Unique owner id.
+        - email(str):
+        - password(str)
+        - clients(ManyToManyField): Clients associated with this owner.
+
+    """
+    owner_id = models.IntegerField(primary_key=True)
+    email = models.CharField(unique=True, max_length=50)
+    password = models.CharField(max_length=20, null=False)
+    clients = models.ManyToManyField(Client)
