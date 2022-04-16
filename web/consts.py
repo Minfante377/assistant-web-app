7d1eecf82b372af06a6b8f61b0819e7a3d126c08
Martin Infante <maleinf09@gmail.com>
2022-04-16 18:27:23 -0300

    Add missing files

---
+++ web/consts.py
@@ -0,0 +1,15 @@
+class Language:
+    MONTH = {
+        'Enero': 'January',
+        'Febrero': 'February',
+        'Marzo': 'March',
+        'Abril': 'April',
+        'Mayo': 'May',
+        'Junio': 'June',
+        'Julio': 'July',
+        'Agosto': 'August',
+        'Septiembre': 'September',
+        'Octubre': 'October',
+        'Noviembre': 'November',
+        'Diciembre': 'December'
+    }
