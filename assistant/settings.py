06d4e426d13f8ba59b2d5c37e199aa5725e081f9
Martin Infante <maleinf09@gmail.com>
2022-10-05 13:56:52 -0300

    Delete key

--- assistant/settings.py
+++ assistant/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v^jjr@0ncj_vwz2i2o1n*d2+c@&m4fouhvqfny^p@-f7u#-!n^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", True)
